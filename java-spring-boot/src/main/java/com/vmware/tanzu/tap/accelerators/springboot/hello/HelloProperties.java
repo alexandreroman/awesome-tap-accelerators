d244f8e5d5a1538641641f1499a2b22b9d9ba1f3
Alexandre Roman <alexandre.roman@broadcom.com>
2024-03-08 10:29:46 +0100

    Clean up

--- java-spring-boot/src/main/java/com/vmware/tanzu/tap/accelerators/springboot/hello/HelloProperties.java
+++ java-spring-boot/src/main/java/com/vmware/tanzu/tap/accelerators/springboot/hello/HelloProperties.java
@@ -1,7 +1,6 @@
 package com.vmware.tanzu.tap.accelerators.springboot.hello;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.hello")
 record HelloProperties(
